e56114248d3e4bef7db569a64a891b137fac8f4b
Jan Pasławski <jpaslawski93@gmail.com>
2025-06-11 20:37:55 +0200

    math functions and integrals methods implemented

--- Integrals/Integrals.c
+++ Integrals/Integrals.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
+#include "math_functions.h"
 
 int main(int argc, char* argv[])
 {
-	printf("hello\n");
-	system("pause");
+    double a, b;
+    int n;
+
+    printf("\n\n");
+    printf("Podaj dolną granicę całkowania: ");
+    scanf_s(" %lf", &a);
+    printf("Podaj górną granicę całkowania: ");
+    scanf_s(" %lf", &b);
+    printf("Podaj liczbę podziałów / prób: ");
+    scanf_s("%d", &n);
+    printf("\n\n");
+
+    printf("Funkcja f(x)=-x^2 :\n");
+    //printf("Metoda prostokątów\n");
+    integral_rectangle(f1_minus_squared_x, a, b, n);
+    //printf("Metoda trapezów\n");
+    integral_trapeze(f1_minus_squared_x, a, b, n);
+    //printf("Metoda Monte-Carlo\n");
+    integral_montecarlo(f1_minus_squared_x, a, b, n);
+    printf("\n");
+
+    printf("Funkcja f(x)=x^3 :\n");
+    //printf("Metoda prostokątów\n");
+    integral_rectangle(f2_cubed_x, a, b, n);
+    //printf("Metoda trapezów\n");
+    integral_trapeze(f2_cubed_x, a, b, n);
+    //printf("Metoda Monte-Carlo\n");
+    integral_montecarlo(f2_cubed_x, a, b, n);
+    printf("\n");
+
+    printf("Funkcja f(x)=ln(x) :\n");
+    //printf("Metoda prostokątów\n");
+    integral_rectangle(f3_ln_x, a, b, n);
+    //printf("Metoda trapezów\n");
+    integral_trapeze(f3_ln_x, a, b, n);
+    //printf("Metoda Monte-Carlo\n");
+    integral_montecarlo(f3_ln_x, a, b, n);
+    printf("\n");
+
+    printf("Funkcja f(x)=1/x :\n");
+    //printf("Metoda prostokątów\n");
+    integral_rectangle(f4_one_over_x, a, b, n);
+    //printf("Metoda trapezów\n");
+    integral_trapeze(f4_one_over_x, a, b, n);
+    //printf("Metoda Monte-Carlo\n");
+    integral_montecarlo(f4_one_over_x, a, b, n);
+    printf("\n\n");
+    system("pause");
+
+    return 0;
 }
 
--- Integrals/integral_functions.c
+++ Integrals/integral_functions.c
@@ -1,13 +1,39 @@
 #include "integral_functions.h"
+#include "math_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void integral_rectangle()
-{
+typedef double (*my_function_ptr)(double);
+
+void integral_rectangle(my_function_ptr f, double LOWER_BOUND, double UPPER_BOUND, int n) {
+    double dx = (UPPER_BOUND - LOWER_BOUND) / n;
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+        double x = LOWER_BOUND + i * dx;
+        sum += f(x);
+    }
+    double result = dx * sum;
+    printf("Metoda prostokątów: %lf\n", result);
 }
 
-void integral_trapeze()
-{
+void integral_trapeze(double (*f)(double), double LOWER_BOUND, double UPPER_BOUND, int n) {
+    double dx = (UPPER_BOUND - LOWER_BOUND) / n;
+    double sum = 0.5 * (f(LOWER_BOUND) + f(UPPER_BOUND));
+    for (int i = 1; i < n; i++) {
+        double x = LOWER_BOUND + i * dx;
+        sum += f(x);
+    }
+    double result = dx * sum;
+    printf("Metoda trapezów: %lf\n", result);
 }
 
-void integral_montecarlo()
-{
+void integral_montecarlo(double (*f)(double), double LOWER_BOUND, double UPPER_BOUND, int n) {
+    srand(time(NULL));
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+        double x = LOWER_BOUND + ((double)rand() / RAND_MAX) * (UPPER_BOUND - LOWER_BOUND);
+        sum += f(x);
+    }
+    double result = (UPPER_BOUND - LOWER_BOUND) * sum / n;
+    printf("Metoda Monte Carlo: %lf\n", result);
 }
